41ab01f3940c1c6d256630c775a6eb28b6910b27
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-08-10 08:56:50 +0300

    rsiheatmap.svelte up to date

--- src/routes/tools/rsiheatmap.svelte
+++ src/routes/tools/rsiheatmap.svelte
@@ -1,19 +1,26 @@
 <script>
   import { onMount } from 'svelte';
+
+  // Tämä muuttuja sisältää API:n perus-URL-osoitteen.
+  // Ensiksi tarkistetaan ympäristömuuttuja 'VITE_BACKEND_URL',
+  // jos sitä ei ole määritetty, käytetään oletuksena 'http://localhost:5002'.
   let heatmapUrl = '';
   let apiBaseUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5002';
 
+  // Koodi, joka suoritetaan, kun komponentti on ladattu.
   onMount(async () => {
     try {
+      // Tehdään pyyntö API:lle heatmap-kuvan hakemiseksi.
       const response = await fetch(`${apiBaseUrl}/heatmap`);
       if (response.ok) {
+        // Jos vastaus on onnistunut, se muunnetaan Blobiksi luomaan väliaikainen URL-osoite.
         const blob = await response.blob();
         heatmapUrl = URL.createObjectURL(blob);
       } else {
-        console.error('Error fetching heatmap:', response.status);
+        console.error('Virhe heatmapin haussa:', response.status);
       }
     } catch (error) {
-      console.error('Fetch error:', error);
+      console.error('Hakupyynnön virhe:', error);
     }
   });
 </script>
@@ -21,9 +28,11 @@
 <main class="widget-container">
   <h1>RSI Heatmap</h1>
   {#if heatmapUrl}
+    <!-- Näytetään heatmap-kuva, jos se on saatavilla -->
     <img src={heatmapUrl} alt="RSI Heatmap" />
   {:else}
-    <p>Loading heatmap...</p>
+    <!-- Näytetään latausviesti, kunnes heatmap on haettu -->
+    <p>Ladataan heatmapia...</p>
   {/if}
 </main>
 
