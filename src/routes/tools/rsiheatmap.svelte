a2751ee9d5c77e618181cb3d54581178a6cf5aee
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-08-10 11:40:07 +0300

    rsiheatmap up to date

--- src/routes/tools/rsiheatmap.svelte
+++ src/routes/tools/rsiheatmap.svelte
@@ -5,7 +5,7 @@
   // Jos ympäristömuuttuja 'VITE_BACKEND_URL' on määritetty, sitä käytetään.
   // Muussa tapauksessa käytetään oletuksena 'http://localhost:5002' paikalliseen kehitykseen.
   let heatmapUrl = '';
-  let apiBaseUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5002';
+  let apiBaseUrl = import.meta.env.VITE_BACKEND_URL || 'https://cauco.up.railway.app';
 
   // Tämä koodi suoritetaan, kun komponentti on ladattu.
   onMount(async () => {
