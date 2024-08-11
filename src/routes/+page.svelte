f987507c30eae111e54333730838e0843ef34d23
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-08-11 09:26:09 +0300

    +page.svelte up to date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
 <script>
+    
+    // Tämä koodi tuo Main-komponentin, joka on Svelte-komponentti ja joka todennäköisesti sisältää pääsisällön tai käyttöliittymän.
+    // Tämä komponentti sijoitetaan suoraan renderöitäväksi sivulle.
+
     import Main from "../components/Main.svelte";
 </script>
 
