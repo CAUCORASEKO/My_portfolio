bb0b3b6725844ca2d20c926dca2ace7f65be275b
CAUCO70 <156064242+CAUCORASEKO@users.noreply.github.com>
2024-08-15 08:53:47 +0300

    Footer icons

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,53 +1,40 @@
 <!-- Alatunnisteosa -->
 <footer id="contact" class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center text-center text-white">
-    <!-- Teksti "Ota yhteyttä minuun" -->
     <p class="px-4 py-2 bg-white text-slate-950 font-medium mb-6">
         Contact
     </p>
-    <!-- Yhteystiedot: sähköposti, GitHub ja LinkedIn -->
     <div class="flex flex-col sm:flex-row gap-8 sm:gap-16 justify-center items-center">
         <div class="flex flex-col items-center">
-            <p><b class="pr-2">E-mail :</b> cauco_dev@gmail.com</p>
-        </div>
-        <div class="flex flex-col items-center">
             <p>
-                <b class="pr-2">GitHub :</b>
-                <a href="https://github.com/CAUCORASEKO" target="_blank" class="text-violet-400 hover:text-violet-600 transition-colors duration-300">
-                    CAUCO
-                    <sup>
-                        <span class="text-xs scale-75 pl-0.5">
-                            <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
-                        </span>
-                    </sup>
-                </a>
+                <i class="fa-solid fa-envelope pr-2"></i>
+                <b class="pr-2">:</b> cauco_dev@gmail.com
             </p>
         </div>
-        <div class="flex flex-col items-center">
-            <p>
-                <b class="pr-2">LinkedIn :</b>
-                <a href="https://www.linkedin.com/in/multimedia3d/" target="_blank" class="text-violet-400 hover:text-violet-600 transition-colors duration-300">
-                    CAUCO
-                    <sup>
-                        <span class="text-xs scale-75 pl-0.5">
-                            <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
-                        </span>
-                    </sup>
-                </a>
-            </p>
+        <div class="flex flex-row items-center gap-4">
+            <a href="https://twitter.com/Manda_coin" target="_blank" class="text-white hover:text-violet-600 transition-colors duration-300">
+                <i class="fa-brands fa-square-x-twitter fa-lg"></i>
+            </a>
+            <a href="https://instagram.com/MandaCoin" target="_blank" class="text-white hover:text-violet-600 transition-colors duration-300">
+                <i class="fa-brands fa-square-instagram fa-lg"></i>
+            </a>
+            <a href="https://linkedin.com/in/yourprofile" target="_blank" class="text-white hover:text-violet-600 transition-colors duration-300">
+                <i class="fa-brands fa-linkedin fa-lg"></i>
+            </a>
+            <a href="https://github.com/CAUCORASEKO" target="_blank" class="text-white hover:text-violet-600 transition-colors duration-300">
+                <i class="fa-brands fa-square-github fa-lg"></i>
+            </a>
         </div>
     </div>
 </footer>
 
-<!-- Selitykset suomeksi
-
-<footer>: Tämä komponentti toimii verkkosivuston alatunnisteosana. Se sisältää yhteystiedot, jotta vierailijat voivat ottaa yhteyttä kehittäjään.
+<!-- Tämä komponentti toimii verkkosivuston alatunnisteosana. Se sisältää yhteystiedot, jotta vierailijat voivat ottaa yhteyttä kehittäjään.
 
 Tyyliluokat:
     py-20 sm:py-32: Asettaa alatunnisteen pystysuoran tilan.
     bg-black: Käyttää mustaa taustaa.
     border-t border-solid border-violet-950: Lisää kiinteän yläreunan, jossa on määritelty väri.
 
-Yhteystiedot-osio: Tämä osio sisältää yhteystiedot, kuten sähköpostiosoitteen, GitHubin ja LinkedInin. Linkit avautuvat uuteen välilehteen (target="_blank").
+Yhteystiedot-osio: Tämä osio sisältää yhteystiedot, kuten sähköpostiosoitteen ja linkit sosiaaliseen mediaan. Linkit avautuvat uuteen välilehteen (target="_blank").
 
 hover:text-violet-600 transition-colors duration-300: Animaatio, joka muuttaa linkkien väriä, kun kursori liikkuu niiden päälle. Transition-colors määrittelee värinvaihdon kestoksi 300 millisekuntia.
 
@@ -56,4 +43,8 @@ flex flex-col sm:flex-row: Käyttää joustavaa asettelua, jossa elementit ovat 
 gap-8 sm:gap-16: Asettaa elementtien välisen välin pienillä (8px) ja suuremmilla (16px) näytöillä.
 
 text-white: Asettaa tekstin värin valkoiseksi.
--->
+
+iconit: Lisää ikoneita sosiaalisen median linkkien viereen käyttäen Font Awesome -kirjastoa. Font Awesome -ikoneille on asetettu fa-lg -luokka, joka suurentaa ikoneita hieman.
+--> 
+
+
